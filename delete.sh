25b580d4f881bd8576d7faab418bf16d0a6617ef
opk <opk@example.com>
2024-01-23 19:31:37 -0700

    Fix the weather Helm

--- delete.sh
+++ delete.sh
@@ -6,6 +6,6 @@ helm delete $(helm list | grep -e  product   -e reviews    -e web   -e postgresq
 kubectl  delete pvc data-postgresql-ha-postgresql-0     \
             data-postgresql-ha-postgresql-1      \
             data-postgresql-ha-postgresql-2     \
-            data-redis-ha-server-0  \
-            data-redis-ha-server-1  \
-            data-redis-ha-server-2
+            data-redis-redis-ha-server-0  \
+            data-redis-redis-ha-server-1  \
+            data-redis-redis-ha-server-2
